5ff4996be3affafca6cfbc1b12576c2ae83d1a70
@au554955 <201606351@post.au.dk>
2023-03-01 21:34:38 +0100

    Added click event to both onclick functions

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,19 @@
 const Home = () => {
 
-  const handleClick = () => {
-    console.log("You pressed the button!");
+  const handleClick = (e) => {
+    console.log("Here's the event: ", e);
   }
 
-  const handleOtherClick = (name) => {
-    console.log("Hello " + name);
+  const handleOtherClick = (name, e) => {
+    console.log("Hello " + name + ", your click event target is: ", e.target);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me!</button>
-      <button onClick={() => {
-        handleOtherClick("Lea");
+      <button onClick={(e) => {
+        handleOtherClick("Lea", e);
       }}>Click me too!</button>
     </div>
   );
