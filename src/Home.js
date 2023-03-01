9ae5597269f7e7848cc468e4dd7f17423c388fba
@au554955 <201606351@post.au.dk>
2023-03-01 21:31:20 +0100

    Added anonymous function for passing parameter

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,17 @@ const Home = () => {
     console.log("You pressed the button!");
   }
 
+  const handleOtherClick = (name) => {
+    console.log("Hello " + name);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me!</button>
+      <button onClick={() => {
+        handleOtherClick("Lea");
+      }}>Click me too!</button>
     </div>
   );
 }
