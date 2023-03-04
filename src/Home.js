42d113ce1c5cb2af6c3f5c8a7c6bcb225e66537a
@au554955 <201606351@post.au.dk>
2023-03-04 15:06:56 +0100

    Added delete blog button and passed as prop

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,15 @@ const Home = () => {
     {title: "Web dev for cool kids", body: "lorem ipsum", author: "Thomas", id: 3},
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog =>blog.id !== id)
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === "Thomas")} title="Thomas' blogs"/>
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === "Thomas")} title="Thomas' blogs" handleDelete={handleDelete}/>
     </div>
   );
 }
