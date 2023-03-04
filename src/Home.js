86fc88198730e1a46eab2e7b138ed992d59b4e78
@au554955 <201606351@post.au.dk>
2023-03-04 14:47:07 +0100

    Added blog entries and map function

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-
+  const [blogs, setBlogs] = useState([
+    {title: "My new website", body: "lorem ipsum", author: "Thomas", id: 1},
+    {title: "Welcome party!", body: "lorem ipsum", author: "Lea", id: 2},
+    {title: "Web dev for cool kids", body: "lorem ipsum", author: "Thomas", id: 3},
+  ]);
 
   return (
     <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
