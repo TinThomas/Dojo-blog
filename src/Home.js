687aead47990a3eb4e28830e3678a215fcdd83bd
@au554955 <201606351@post.au.dk>
2023-03-04 15:34:58 +0100

    Removed delete button

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,6 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog =>blog.id !== id)
-    setBlogs(newBlogs);
-  }
-
   useEffect(() => {
     fetch("http://localhost:8000/blogs").then(res => {
       return res.json();
@@ -20,8 +15,8 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
-      {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Thomas")} title="Thomas' blogs" handleDelete={handleDelete}/>}
+      {blogs && <BlogList blogs={blogs} title="All blogs"/>}
+      {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Thomas")} title="Thomas' blogs"/>}
     </div>
   );
 }
