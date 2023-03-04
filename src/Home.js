029a008bfaa6f1347b483a1d1736b5f2e2323078
@au554955 <201606351@post.au.dk>
2023-03-04 14:39:50 +0100

    Removed all the fancy stuff again lol

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,10 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(26);
 
-  const handleClick = () => {
-    setName("Luigi");
-    setAge(27);
-  }
-
-  const handleOtherClick = (name, e) => {
-    console.log("Hello " + name + ", your click event target is: ", e.target);
-  }
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>Click me!</button>
-      <button onClick={(e) => {
-        handleOtherClick("Lea", e);
-      }}>Click me too!</button>
     </div>
   );
 }
