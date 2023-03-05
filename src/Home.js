ffb582c26b31296b62a34eb9b1de36a9540a8c7e
@au554955 <201606351@post.au.dk>
2023-03-05 19:58:37 +0100

    Added loading message

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs").then(res => {
@@ -10,11 +11,13 @@ const Home = () => {
     }).then((data) =>{
       console.log(data);
       setBlogs(data);
+      setIsLoading(false);
     })
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs"/>}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Thomas")} title="Thomas' blogs"/>}
     </div>
