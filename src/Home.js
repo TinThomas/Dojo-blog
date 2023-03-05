9154b10ba59dcb2a10d7af1d2f2557a0d65dff43
@au554955 <201606351@post.au.dk>
2023-03-05 21:38:23 +0100

    Added custom useFetch hook

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,14 @@
 import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs").then(res => {
-      if(!res.ok){
-        throw Error("Could not fetch data for resource");
-      }
-      return res.json();
-    }).then((data) =>{
-      console.log(data);
-      setBlogs(data);
-      setIsLoading(false);
-      setError(null);
-    }).catch((err) => {
-      setError(err.message);
-      setIsLoading(false);
-    })
-  }, []);
+  const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
-      {isLoading && <div>Loading...</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs"/>}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Thomas")} title="Thomas' blogs"/>}
     </div>
