154081ad8928ff4fde30eb7ae566e5eef48ab19e
@au554955 <201606351@post.au.dk>
2023-03-07 22:38:33 +0100

    Added a new blog Form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Thomas");
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+
+        <label>Blog author</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Thomas">Thomas</option>
+          <option value="Lea">Lea</option>
+        </select>
+      </form>
     </div>
    );
 }
