a41ba0c5e7c50862e4162a067613191bed2c9ebe
@au554955 <201606351@post.au.dk>
2023-03-07 22:48:25 +0100

    Added pending state for adding blogs

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,13 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Thomas");
+  const [isPending, setIsPending] = useState(false)
 
   const handleSubmit = (e) =>{
     e.preventDefault();
     const blog = {title, body, author}
+
+    setIsPending(true);
 
     fetch("http://localhost:8000/blogs", {
       method: "POST",
@@ -15,6 +18,7 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log("New blog added")
+      setIsPending(false);
     })
   }
 
@@ -45,7 +49,8 @@ const Create = () => {
           <option value="Lea">Lea</option>
         </select>
 
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
    );
